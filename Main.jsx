5b8304dd239f5e0854c599d049e06563b669b43c
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-05-15 23:55:04 +0100

    file renamed... App.jsx to Main.jsx

---
+++ Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App.jsx"
+
+const root = ReactDom.createRoot(document.querySelector("#root"));
+
+root.render(<App/>);
+ 
