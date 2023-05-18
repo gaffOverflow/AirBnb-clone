a1df0fb6b672f4877e4e94adc05137f06a8051da
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-05-19 00:49:18 +0100

    new changes added... lord icon added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import "../index.css"
+import "../index.css";
+import lottie from "lottie-web";
+import { defineElement } from "lord-icon-element";
 
-import AirBnblogo from "../assets/icons/AirBnbLogo.png"
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation);
 
-function NavBar(){
-  return(
-    <nav className="py-6 px-8 shadow-md">
+import AirBnblogo from "../assets/icons/AirBnbLogo.png";
+
+function NavBar() {
+  return (
+    <nav className="flex items-center justify-between px-8 py-6 shadow-md">
       <img src={AirBnblogo} alt="AirBnb Logo" />
+
+      <lord-icon
+        src="https://cdn.lordicon.com/flvisirw.json"
+        trigger="loop-on-hover"
+        delay="20"
+        colors="primary:#646e78,secondary:#4bb3fd,tertiary:#ebe6ef"
+        style={{ width: "10%", height: "" }}
+      ></lord-icon>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
