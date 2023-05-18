3285889860f2be586faea31177cd2e0e3d6e5cb4
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-05-18 02:03:20 +0100

    AirBnb Mini project using react props and Map to populate the card carousel with data

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,21 +2,35 @@ import React from "react";
 import "../index.css";
 
 import StarIcon from "../assets/icons/Star-icon.png";
+import Data from "./CardData.jsx";
 
 function Cards(props) {
+
+let tag
+if(props.stock === 0){
+  tag = "SOLD OUT"
+} else if(props.location === "Online"){
+  tag = "ONLINE"
+}
+
   return (
     <div>
-      <figure>
+      <figure className="relative">
+        {tag && (
+          <div className="absolute left-2 top-2 rounded-md bg-white px-2 py-0.5">
+            {tag}
+          </div>
+        )}
         <img
-          src={props.img}
+          src={props.coverImg}
           alt="Card image"
           className="inline-block h-52 w-44 md:h-72 md:w-56"
         />
         <figcaption className="flex items-center gap-1 pt-2 text-lg">
           <img src={StarIcon} alt="Start Icon" className="w-5" />
-          <span className="font-semibold">{props.rating}</span>
+          <span className="font-semibold">{props.stats.rating}</span>
           <span className="text-[#918E9B]">
-            ({props.reviewCount}).{props.country}
+            ({props.stats.reviewCount}).{props.country}
           </span>
         </figcaption>
       </figure>
@@ -32,39 +46,21 @@ function Cards(props) {
       </h4>
     </div>
   );
-
-
-  
 }
 
 function CardSection() {
+  const DataList = Data.map((entries) => {
+    return (
+      <Cards
+      key={entries.id}
+       {...entries}
+      />
+    );
+  });
   return (
     <section className="overflow-x-scroll">
       <div className=" flex min-w-max justify-center gap-4 px-10 py-10 sm:px-16 md:gap-10 lg:px-16">
-        <Cards
-          img="./image/card1.png"
-          rating={`5.0`}
-          reviewCount={6}
-          country="Nigeria"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
-        <Cards
-          img="./image/card2.png"
-          rating={`5.0`}
-          reviewCount={6}
-          country="Nigeria"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
-        <Cards
-          img="./image/card3.png"
-          rating={`5.0`}
-          reviewCount={6}
-          country="Nigeria"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />{" "}
+        {DataList}
       </div>
     </section>
   );
